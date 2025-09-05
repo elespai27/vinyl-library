eb3d961895853f5e78e672635575e00459de6c6c
elespai27 <elespai27@gmail.com>
2025-09-05 16:36:45 +0200

    Angular/fire update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { eyeOutline, eyeOffOutline } from 'ionicons/icons';
 
@@ -11,6 +19,14 @@ import { environment } from './environments/environment';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -20,10 +36,28 @@ bootstrapApplication(AppComponent, {
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'vinyl-library-7b7c5',
+        appId: '1:91528022194:web:ce87d371671b364ee6a1ab',
+        storageBucket: 'vinyl-library-7b7c5.firebasestorage.app',
+        apiKey: 'AIzaSyCVdiz-B0kfjXQQkSFcpxn9sswEUsVz1as',
+        authDomain: 'vinyl-library-7b7c5.firebaseapp.com',
+        messagingSenderId: '91528022194',
+        measurementId: 'G-PQCNDX703X',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
   ],
 });
 
 addIcons({
   'eye-outline': eyeOutline,
-  'eye-off-outline': eyeOffOutline
+  'eye-off-outline': eyeOffOutline,
 });
